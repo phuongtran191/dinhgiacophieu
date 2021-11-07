f38535370c8c611c5929aa9a60d7109a8ba2c608
phuongtran191 <phuongtran.jdan@gmail.com>
2021-11-07 20:40:19 +0700

    first commit

---
+++ js/price_valuation.js
@@ -0,0 +1,40 @@
+"use strict";
+var $ = function(id) {
+  return document.getElementById(id);
+};
+
+function validate() {
+
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function calculate() {
+  let in1 = parseFloat($("income1").value);
+  let in2 = parseFloat($("income2").value);
+  let in3 = parseFloat($("income3").value);
+  let in4 = parseFloat($("income4").value);
+  let vol = parseInt($("volume").value.replace(/,/g, ''));
+  console.log("🚀 ~ file: price_valuation.js ~ line 22 ~ calculate ~ vol", vol)
+  let pe1 = parseFloat($("pe1").value);
+  let pe2 = parseFloat($("pe2").value);
+  // Do calculate
+  const EPS = ((in1 + in2 + in3 + in4)*Math.pow(10, 9)/vol).toFixed(3);
+  let price1 = 0, price2 = 0;
+  price1 = pe1*EPS*1000;
+  price2 = pe2*EPS*1000;
+  // Format number VNĐ
+  const price1String = (Math.round(price1/100)*100).toLocaleString();
+  const price2String = (Math.round(price2/100)*100).toLocaleString();
+  // Add string into element
+  let html = "";
+  html = "Cổ phiếu bạn đang xét có giá trị từ <b>" + price1String + "</b> (VNĐ) đến <b>" + price2String + "</b> (VNĐ).";
+  $("result").innerHTML = html;
+}
+
+window.onload = function() {
+  $("valuate").onclick = calculate;
+  $("income1").focus();
+};
